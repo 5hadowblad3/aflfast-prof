5ae35719c72cd7bbd59f0d7d1edc90a69c2f1462
Shadowblade <hhuangaz@cse.ust.hk>
2021-09-25 20:48:03 +0800

    update logging

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -7376,7 +7376,7 @@ EXP_ST void setup_dirs_fds(void) {
 
     fprintf(plot_file, "# unix_time, cycles_done, cur_path, paths_total, "
                        "pending_total, pending_favs, map_size, unique_crashes, "
-                       "unique_hangs, max_depth, execs_per_sec,"
+                       "unique_hangs, max_depth, execs_per_sec, "
                        "total_fuzz, total_select, total_energy_used\n");
     /* ignore errors */
 
